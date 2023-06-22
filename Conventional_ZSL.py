101565bdda605cbc6d249be7954334d065b94f17
william-heyden <137411636+william-heyden@users.noreply.github.com>
2023-06-22 15:33:29 +0200

    Updated Conventional_ZSL.py

---
+++ Conventional_ZSL.py
@@ -0,0 +1,19 @@
+import numpy as np
+import scipy
+from scipy import spatial
+import load_data as ld
+from sae_helper import SAE, acc_zsl, NormalizeFae
+
+
+# Load data
+X_tr, X_te, S_tr, S_te, Y_te, test_cls, Y_tr, S_te_all = ld.cub(int_proj=True)
+
+# Solve for W
+lamb  = 500000;
+W=SAE(X_tr.T,S_tr.T,lamb).T
+
+# Projecting from semantic to visual space 
+S_tr_gt_all=NormalizeFea(S_te_all.T,2).T
+dist = 1 - spatial.distance.cdist(X_te,S_tr_gt_all.dot(W.T),'cosine')
+
+print('Accuracy: {}'.format(acc_zsl(dist, test_cls, Y_te)*100))
